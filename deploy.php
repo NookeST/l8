f4de5c8dfa7600fda6d0262cbf98b7883d388852
Sergey A <sergey.v.akkuzin@gmail.com>
2021-06-22 16:29:20 +0300

    wip

--- deploy.php
+++ deploy.php
@@ -4,18 +4,9 @@ namespace Deployer;
 
 // Include the Laravel & rsync recipes
 require 'recipe/laravel.php';
-require 'recipe/rsync.php';
 
 set('application', 'l8');
-set('ssh_multiplexing', true); // Speeds up deployments
-
-set(
-    'rsync_src',
-    function () {
-        return __DIR__; // If your project isn't in the root, you'll need to change this.
-    }
-);
-
+set('repository', 'git@github.com:NookeST/l8.git');
 
 // Hosts
 host('l8')
@@ -23,17 +14,5 @@ host('l8')
     ->stage('staging') // Deployment stage (production, staging, etc)
     ->user('avtomaty6') // SSH user
     ->set('deploy_path', '~/public_html'); // Deploy path
-
-
-// Set up a deployer task to copy secrets to the server.
-// Since our secrets are stored in GitHub, we can access them as env vars.
-task(
-    'deploy:secrets',
-    function () {
-        file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
-        upload('.env', get('deploy_path') . '/shared');
-    }
-);
-
 
 after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
